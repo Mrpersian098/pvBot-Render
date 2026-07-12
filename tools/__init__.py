bdadda1d79081ca2af7aae1c794ae97897bafe0e
Mrpersian098 <67864551+Mrpersian098@users.noreply.github.com>
2026-07-12 15:39:25 +0330

    Create __init__.py

---
+++ tools/__init__.py
@@ -0,0 +1,25 @@
+from tools.crypto import router as crypto_router
+from tools.gold import router as gold_router
+from tools.unit_converter import router as unit_converter_router
+from tools.bmi import router as bmi_router
+from tools.calorie import router as calorie_router
+from tools.calendar_h import router as calendar_router
+from tools.phone_info import router as phone_info_router
+from tools.random_text import router as random_text_router
+from tools.games import router as games_router
+from tools.url_shortener import router as url_shortener_router
+from tools.hafez import router as hafez_router
+
+tools_routers = [
+    crypto_router,
+    gold_router,
+    unit_converter_router,
+    bmi_router,
+    calorie_router,
+    calendar_router,
+    phone_info_router,
+    random_text_router,
+    games_router,
+    url_shortener_router,
+    hafez_router,
+]
